c0f42edabc3617a53f967e1408fabd3a30f8ac38
Abdusin <Abdusin65@gmail.com>
2021-12-25 21:47:34 +0300

    System Updated

--- bin/flucreator.dart
+++ bin/flucreator.dart
@@ -48,6 +48,14 @@ void createScreen(List<String> args) async {
     return name;
   }
 
+  String classNameFormatter(String raw) {
+    var name = '';
+    name = raw.replaceAllMapped(RegExp(r'_[a-z]'), (match) {
+      return match.group(0).substring(1, 2).toUpperCase() + match.group(0).substring(2);
+    });
+    return name;
+  }
+
   var noController = args.contains('--no-controller');
   if (args.last.isNotEmpty && !args.last.contains('--create=')) {
     name = fileNameFormatter(args.last);
@@ -67,13 +75,15 @@ void createScreen(List<String> args) async {
   if (name.contains('screen')) {
     name = name.replaceAll('screen', '');
   }
+  var screenName = classNameFormatter(name) + 'Screen';
   var screenFile = await File('lib/screens/${name}_screen.dart').create(recursive: true);
   if (!noController) {
+    var controllerName = classNameFormatter(name) + 'ScreenController';
     var controllerFile = await File('lib/controllers/$name' '_controller.dart').create(recursive: true);
-    screenControllerSetter(controllerFile, name + 'ScreenController');
-    screenSetter(screenFile, packageName, name, name + 'ScreenController');
+    screenControllerSetter(controllerFile, controllerName);
+    screenSetter(screenFile, packageName, screenName, controllerName);
   } else {
-    screenSetter(screenFile, packageName, name);
+    screenSetter(screenFile, packageName, screenName);
   }
 
   exit(0);
